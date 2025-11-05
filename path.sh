d92a4996bf1033a13272e1311ef6bc13c285b778
Klonesingh <kloudprepare@gmail.com>
2025-11-05 15:17:42 +0530

    test

--- path.sh
+++ path.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
-echo "User Name":$User
-echo "Path":$path
-echo "home directory":$home
-echo "shell name":$shell
+echo "User Name":$USER
+echo "Path":$PATH
+echo "home directory":$HOME
+echo "shell name":$SHELL
 echo "shell name":$0
+echo "ls -l|$(wc -l)"
